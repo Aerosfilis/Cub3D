6c7943e58de3982ff6c4b322d98abb143f789223
Eona <calliste.bugnon@gmail.com>
2020-02-17 23:54:07 +0100

    Added fps based processing and corrected makefile.

--- Makefile
+++ Makefile
@@ -5,15 +5,17 @@ SRC		=
 OBJ		=	$(SRC:.c=.o)
 
 CC		=	gcc
-CFLAGS	=	-Wall -Werror -Wextra -Isrcs
+CFLAGS	=	-Wall -Werror -Wextra -Isrcs -fsanitize=address
 
 OS		=	$(shell uname -s)
 ifeq ($(OS), Linux)
 	MLXDIR	=	mlx_linux/
-	CFLAGS	+=	-I$(MLXDIR) -L$(MLXDIR) -lmlx -lXext -lX11
+	CFLAGS	+=	-I$(MLXDIR)
+	GFLAGS	=	-L$(MLXDIR) -lmlx -I$(MLXDIR) -lXext -lX11
 else
 	MLXDIR	=	mlx_opengl/
-	CFLAGS	+=	-I$(MLXDIR) -L$(MLXDIR) -lmlx -framework OpenGL -framework AppKit
+	CFLAGS	+=	-I$(MLXDIR)
+	GFLAGS	=	-L$(MLXDIR) -lmlx -I$(MLXDIR) -framework OpenGL -framework AppKit
 endif
 
 OBJS/%.o:	%.c
@@ -22,16 +24,23 @@ OBJS/%.o:	%.c
 
 all:		$(NAME)
 
+$(NAME):	$(addprefix srcs/, $(OBJS))
+			make -C ./$(MLXDIR) --no-print-directory
+			$(CC) $(CFLAGS) -o $(NAME) $(GFLAGS)
+clean:
+			rm -rf objs
+
+fclean:		clean
+			rm $(NAME)
+
+re:			fclean all
+
 os:
 			@echo "$(OS)"
 
 cflags:	
 			@echo "$(CFLAGS)"
 
-$(NAME):	$(addprefix srcs/, $(OBJS))
-			make -C ./$(MLXDIR) --no-print-directory
-			$(CC) $(CFLAGS) -o $(NAME)
-
 test:
 			make -C ./$(MLXDIR) --no-print-directory
-			$(CC) $(CFLAGS) -o mlx_test mlx_test.c
+			$(CC) $(CFLAGS) -o mlx_test mlx_test.c $(GFLAGS)
--- mlx_test.c
+++ mlx_test.c
@@ -1,14 +1,20 @@
 #include "mlx.h"
 #include <stdio.h>
+#include <limits.h>
+#include <time.h>
 
 #define WSX 600
 #define WSY 450
+#define MAX_KEY 512
+#define TARGET_FPS 120
 
 #ifdef __linux__
-# define KW
-# define KA
-# define KS
-# define KD
+# define KW 119
+# define KA 97
+# define KS 115
+# define KD 100
+# define SH 65505
+# define ES 65307
 #else
 # define KW 13
 # define KA 0
@@ -20,63 +26,91 @@
 
 typedef struct	s_mlx_struct
 {
-	void	*ptr;
-	void	*win;
-	int		k_press[512];
+	void			*ptr;
+	void			*win;
+	unsigned short	k_press[USHRT_MAX];
 }				t_mlx_struct;
+
+int				get_fps()
+{
+	static clock_t	last=0;
+	clock_t			current=clock(), total=CLOCKS_PER_SEC/(current-last);
+	last = current;
+	(void)total;
+	return total;
+}
+
 
 int	key_press_event(int keycode, t_mlx_struct *mlx)
 {
-	mlx->k_press[keycode] = 1;
 	printf("Press:   %d\n", keycode);
+	mlx->k_press[keycode] = 1;
+	if (keycode == KW && mlx->k_press[KS]) mlx->k_press[KS] = 2;
+	else if (keycode == KS && mlx->k_press[KW]) mlx->k_press[KW] = 2;
+	else if (keycode == KA && mlx->k_press[KD]) mlx->k_press[KD] = 2;
+	else if (keycode == KD && mlx->k_press[KA]) mlx->k_press[KA] = 2;
 	return 0;
 }
 
 int	key_release_event(int keycode, t_mlx_struct *mlx)
 {
-	mlx->k_press[keycode] = 0;
 	printf("Release: %d\n", keycode);
+	if (keycode > USHRT_MAX || keycode < 0) return 0;
+	mlx->k_press[keycode] = 0;
+	if (keycode == KW && mlx->k_press[KS]) mlx->k_press[KS] = 1;
+	else if (keycode == KS && mlx->k_press[KW]) mlx->k_press[KW] = 1;
+	else if (keycode == KA && mlx->k_press[KD]) mlx->k_press[KD] = 1;
+	else if (keycode == KD && mlx->k_press[KA]) mlx->k_press[KA] = 1;
 	return 0;
 }
 
 int	loop_event(t_mlx_struct *mlx)
 {
-	static int	x=WSX/2-5, y=WSY/2-5, color=0x000000FF, cycle=0;
-	int					speed, px, py, i, j;
+	static int			x=WSX/2-5, y=WSY/2-5, color=0x000000FF, cycle=0;
+	static clock_t		last=0;
+	int					speed, px, py, i, j, fps;
+	clock_t				current;
+	char				fps_str[] = "   ";
 
-	speed = mlx->k_press[SH] ? 2 : 1;
-	if (mlx->k_press[KD]) x += speed;
-	else if (mlx->k_press[KA]) x -= speed;
-	if (mlx->k_press[KS]) y += speed;
-	else if (mlx->k_press[KW]) y -= speed;
+	speed = mlx->k_press[SH] ? (double)(200/TARGET_FPS)*2 : (200/TARGET_FPS);
+	if (mlx->k_press[KD] == 1) x += speed;
+	else if (mlx->k_press[KA] == 1) x -= speed;
+	if (mlx->k_press[KS] == 1) y += speed;
+	else if (mlx->k_press[KW] == 1) y -= speed;
 
+	//*
 	switch (cycle)
 	{
 		case 0:
-			color += 0x000100;
-			if ((color & 0x00FF00) == 0x00FF00) cycle++;
+			color += 0x000100 * (200/TARGET_FPS);
+			if ((color & 0x00FE00) == 0x00FE00) cycle++;
 			break;
 		case 1:
-			color -= 0x000001;
-			if ((color & 0x0000FF) == 0) cycle++;
+			color -= 0x000001 * (200/TARGET_FPS);
+			if ((color & 0x0000FE) == 0) cycle++;
 			break;
 		case 2:
-			color += 0x010000;
-			if ((color & 0xFF0000) == 0xFF0000) cycle++;
+			color += 0x010000 * (200/TARGET_FPS);
+			if ((color & 0xFE0000) == 0xFE0000) cycle++;
 			break;
 		case 3:
-			color -= 0x000100;
-			if ((color & 0x00FF00) == 0) cycle++;
+			color -= 0x000100 * (200/TARGET_FPS);
+			if ((color & 0x00FE00) == 0) cycle++;
 			break;
 		case 4:
-			color += 0x000001;
-			if ((color & 0x0000FF) == 0x0000FF) cycle++;
+			color += 0x000001 * (200/TARGET_FPS);
+			if ((color & 0x0000FE) == 0x0000FE) cycle++;
 			break;
 		case 5:
-			color -= 0x010000;
-			if ((color & 0xFF0000) == 0) cycle = 0;
+			color -= 0x010000 * (200/TARGET_FPS);
+			if ((color & 0xFE0000) == 0) cycle = 0;
 			break;
 	}
+	//*/(void)cycle;
+
+	current = clock();
+	while (CLOCKS_PER_SEC/(current-last) > TARGET_FPS) current = clock();
+	last = current;
 
 	mlx_clear_window(mlx->ptr, mlx->win);
 	for (i=0; i<10; i++)
@@ -88,6 +122,17 @@ int	loop_event(t_mlx_struct *mlx)
 			if (py<0) py+=WSY;
 			mlx_pixel_put(mlx->ptr, mlx->win, px, py, color);
 		}
+	//*
+	fps = get_fps();
+	i = (fps>99) + 1;
+	while (i>=0)
+	{
+		fps_str[i] = fps%10 + 48;
+		fps /= 10;
+		i--;
+	}
+	mlx_string_put(mlx->ptr, mlx->win, 2, 11, 0xFFFFFF, fps_str);
+	//*/
 	return 0;
 }
 
@@ -95,12 +140,13 @@ int	main()
 {
 	int				i, j;
 	t_mlx_struct	mlx;
-
-	for (i=0; i<512; i++) mlx.k_press[i] = 0;
+	
+	get_fps();
+	for (i=0; i<USHRT_MAX; i++) mlx.k_press[i] = 0;
 
 	mlx.ptr = mlx_init();
 	mlx.win = mlx_new_window(mlx.ptr, WSX, WSY, "mlx test");
-	mlx_do_key_autorepeatoff(mlx.ptr);
+	mlx_do_key_autorepeaton(mlx.ptr);
 	
 	for (i=0; i<10; i++)
 		for (j=0; j<10; j++)
@@ -109,7 +155,6 @@ int	main()
 	mlx_hook(mlx.win, 2, 1, key_press_event, &mlx);
 	mlx_hook(mlx.win, 3, 2, key_release_event, &mlx);
 	mlx_loop_hook(mlx.ptr, loop_event, &mlx);
-	printf("test\n");
 	
 	mlx_loop(mlx.ptr);
 
